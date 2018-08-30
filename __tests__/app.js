e3b86e2b69482741ff8c2b96e0989861e7af9666
Ricardo <ricardoplouis@gmail.com>
2018-08-30 18:00:25 -0500

    Increase test coverage

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,15 +4,54 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const formatter = require('../generators/app/formatter');
 
+const defaultVals = {
+  componentName: 'Test',
+  componentType: 'class',
+  linter: 'none',
+  JSX: true,
+  es6: true
+};
+
 describe('generator-simple-rc:app', () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ componentName: 'Test', componentType: 'class' });
+      .withPrompts(defaultVals);
   });
 
   it('creates files', () => {
     assert.file(['Test.js']);
+  });
+});
+
+describe('generator-simple-rc:airbnb-linter', () => {
+  beforeAll(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts(
+        Object.assign(defaultVals, { componentName: 'AirFun', linter: 'airbnb' })
+      );
+  });
+
+  it('creates files', () => {
+    assert.file(['AirFun.js']);
+  });
+});
+
+describe('generator-simple-rc:no_es6', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts(
+      Object.assign(defaultVals, {
+        componentName: 'ClassNo',
+        linter: 'none',
+        es6: false,
+        JSX: true
+      })
+    );
+  });
+
+  it('creates files', () => {
+    assert.file(['ClassNo.js']);
   });
 });
 
